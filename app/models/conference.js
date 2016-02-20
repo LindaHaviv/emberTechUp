d6933674890348a5cd2f2eda410a311e6f397d2b
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-20 17:04:06 -0500

    ready for bootswatch

--- app/models/conference.js
+++ app/models/conference.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  location: DS.attr('string'),
   topicId: DS.attr('integer'),
   date: DS.attr('date')
  
