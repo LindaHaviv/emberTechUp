0f3d678d38dfe2fa251d9bd6a09b98328d88a365
LindaHaviv <linda.haviv@gmail.com>
2016-02-20 16:55:04 -0500

    added the associations between classes and deleted join table

--- app/models/topic.js
+++ app/models/topic.js
@@ -2,8 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-   conferences: DS.hasMany('conference')
+  conferences: DS.hasMany('conference')
 });
-
-// add this in ember
-//   has_many :user, through: :conferences
