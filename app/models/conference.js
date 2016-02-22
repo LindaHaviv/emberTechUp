168823fead0c82eba9ab62206128f14388a4a15c
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-22 00:10:07 -0500

    showed creators on conferences page

--- app/models/conference.js
+++ app/models/conference.js
@@ -3,9 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   topicId: DS.attr('integer'),
-  date: DS.attr('date'),
-  // creator: DS.belongsTo('user', {async: true}),
+  date: DS.attr('date', {defaultValue: new Date()}),
+  creator: DS.belongsTo('user', {async: true}),
   topic: DS.belongsTo('topic', {async: true}),
-  attendees: DS.hasMany('user', {async: true})
+  // attendees: DS.hasMany('user', {async: true})
  
 });
