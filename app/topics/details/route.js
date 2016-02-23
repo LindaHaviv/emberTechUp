ef05e5a37c1f0f6f90d531e62adb30e6c6fdeaa9
chrisbradshaw3 <christopherbradshaw11@gmail.com>
2016-02-22 19:36:10 -0500

    added drop down

--- app/topics/details/route.js
+++ app/topics/details/route.js
@@ -5,3 +5,8 @@ export default Ember.Route.extend({
     return this.store.findRecord('topic', params.id);
   }
 });
+
+
+
+
+
---
+++ app/topics/index/route.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // model(){
+  //   return [{name: "Ember.js", status: 1},
+  //   {name: "React.js", status: 2},
+  //   {name: "Angular.js", status: 3}];
+  //   }
+  selectedTopic: null,
+  // topics: ["Ember.js", "React.js", "Angular.js"]  
+    
+});
